ad31a209fbe5beac2cf8628255add4d3d8d37035
Halie Hall <halierae17@outlook.com>
2021-04-21 23:37:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.map!{|name| name.capitalize}.join 
+  array.map!{|name| name.capitalize}.join("! ")
 end
 
 def long_planeteer_calls# code an argument here
