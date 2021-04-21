8d49655d645e1d991879e75c5781ee2fef2ed3e2
Halie Hall <halierae17@outlook.com>
2021-04-21 23:17:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,6 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     new_arr << "#{index + 1}. #{name}"
   end 
-  new_arr
 end
 
 def summon_captain_planet# code an argument here
