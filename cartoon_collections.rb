c34e6bbb9b26ed19e0831f4f78628d8de5e72e50
Halie Hall <halierae17@outlook.com>
2021-04-21 23:50:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
-  new_arr = []
-  dwarves.each_with_index do |name, i|
-    new_arr << "#{i + 1}."
+  index = 0 
+  dwarves.each_with_index do |name, index|
+    puts "#{index = 1}. #{name} "
   end
 end
 
