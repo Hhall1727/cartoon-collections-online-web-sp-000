bc1d4f7ec30485e44f200cc7227518dddd1d9f75
Halie Hall <halierae17@outlook.com>
2021-04-21 23:41:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
   array.map!{|name| name.capitalize}.join("! ")
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
